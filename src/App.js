766b41a96153fd2e2a51aacbd1b951d36d8817f3
Simon Abrams <simon@simonabrams.com>
2021-08-07 12:57:04 -0400

    update about copy

--- src/App.js
+++ src/App.js
@@ -39,11 +39,17 @@ class App extends React.Component {
 
         <div className="about">
           <h1>Portfolio</h1>
-          The work shown in this gallery represents a few examples of the types
-          of projects we specialize in: reproducing Park Slope and Ditmas Park
-          historical entrance doors; intricate parquet floor border patterns;
-          kitchen and bathroom remodels; matched exterior woodwork, and fine
-          interior architectural details.
+          <p>
+            The work shown in this gallery represents a few examples of the
+            types of projects we specialize in: reproducing Park Slope and
+            Ditmas Park historical entrance doors; intricate parquet floor
+            border patterns; kitchen and bathroom remodels; matched exterior
+            woodwork, and fine interior architectural details.
+          </p>
+          <p>
+            We know these houses; every day we do what others say can't be done
+            anymore.
+          </p>
         </div>
 
         <Gallery
