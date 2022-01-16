244091730cdf00b8c847772f6409677e8101f6c8
Simon Abrams <simon@simonabrams.com>
2022-01-15 22:11:43 -0500

    update image gallery

--- src/photos.js
+++ src/photos.js
@@ -1,194 +1,320 @@
 export const photos = [
-	{
-		src: './images/jwebber-woodworking-01.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-01.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 1',
-		// tags: [{ value: 'kitchens', title: 'kitchens'}]
-	},
-	{
-		src: './images/jwebber-woodworking-02.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-02.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-03.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-03.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-04.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-04.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 185,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-05.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-05.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-06.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-06.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-07.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-07.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-08.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-08.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-09.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-09.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-10.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-10.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-11.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-11.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-12.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-12.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-13.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-13.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-14.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-14.jpg',
-		thumbnailWidth: 140,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-15.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-15.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-16.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-16.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-17.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-17.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-18.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-18.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-19.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-19.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-20.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-20.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-21.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-21.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-22.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-22.jpg',
-		thumbnailWidth: 135,
-		thumbnailHeight: 180,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-23.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-23.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-27.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-27.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-24.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-24.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-25.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-25.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	},
-	{
-		src: './images/jwebber-woodworking-26.jpg',
-		thumbnail: './images/thumbnails/jwebber-woodworking-26.jpg',
-		thumbnailWidth: 180,
-		thumbnailHeight: 135,
-		// caption: 'image 2'
-	}
-]
+  {
+    src: "./images/jwebber-woodworking-01.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-01.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 1',
+    // tags: [{ value: 'kitchens', title: 'kitchens'}]
+  },
+  {
+    src: "./images/jwebber-woodworking-02.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-02.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-03.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-03.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-04.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-04.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 185,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-05.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-05.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-06.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-06.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-07.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-07.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-08.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-08.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-09.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-09.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-10.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-10.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-11.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-11.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-12.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-12.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-13.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-13.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-14.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-14.jpg",
+    thumbnailWidth: 140,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-15.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-15.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-16.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-16.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-17.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-17.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-18.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-18.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-19.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-19.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-20.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-20.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-21.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-21.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-22.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-22.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-23.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-23.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-27.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-27.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-24.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-24.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-25.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-25.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-26.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-26.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-40.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-40.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-41.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-41.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 120,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-42.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-42.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-43.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-43.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-44.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-44.jpg",
+    thumbnailWidth: 152,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-45.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-45.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-46.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-46.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-47.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-47.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-48.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-48.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-49.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-49.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-50.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-50.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-51.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-51.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-52.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-52.jpg",
+    thumbnailWidth: 180,
+    thumbnailHeight: 135,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-53.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-53.jpg",
+    thumbnailWidth: 79,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-54.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-54.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-55.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-55.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-56.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-56.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+  {
+    src: "./images/jwebber-woodworking-57.jpg",
+    thumbnail: "./images/thumbnails/jwebber-woodworking-57.jpg",
+    thumbnailWidth: 135,
+    thumbnailHeight: 180,
+    // caption: 'image 2'
+  },
+];
 
 export default photos;
